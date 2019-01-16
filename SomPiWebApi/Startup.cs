4067f3f00d789374331fd9bf141cd60bf552ddf9
Rainson <rene.steen123@googlemail.com>
2019-01-16 15:01:37 +0100

    ..

--- SomPiWebApi/Startup.cs
+++ SomPiWebApi/Startup.cs
@@ -17,7 +17,7 @@ namespace SomPiWebApi
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-        }
+        } 
 
         public IConfiguration Configuration { get; }
 
